d01616b3293c58da2cd66e07237301f3f5f7f0ba
sungyun <sungyun5423@gmail.com>
2020-09-18 00:06:55 +0900

    9/17

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <App />,
   document.getElementById('root'));
